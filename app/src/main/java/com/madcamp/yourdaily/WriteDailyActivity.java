c6eae9895a4e4869cd8518807e3b82ddf4820aea
Taehyung Kim <paulkth2@kaist.ac.kr>
2019-01-14 22:26:50 +0900

    Merge branch 'master' of https://github.com/paulkth2/YourDaily
    # Conflicts:
    #	build.gradle

--- app/src/main/java/com/madcamp/yourdaily/WriteDailyActivity.java
+++ app/src/main/java/com/madcamp/yourdaily/WriteDailyActivity.java
@@ -1,23 +1,91 @@
 package com.madcamp.yourdaily;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteDailyActivity extends AppCompatActivity {
     private static final String TAG = "WriteDailyActivity";
     private static int ACTIVITY_NUM = 2;
     private Context mContext = WriteDailyActivity.this;
 
+    private GridView writtableGrid;
+
+    private ArrayList<PreDaily> preDailies;
+    private ArrayList<String> keys;
+
+    private FirebaseAuth mAuth;
+
+    final ArrayList<Uri> imgURLs = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_daily);
+
+        writtableGrid = (GridView) findViewById(R.id.writtableGrid);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        new FirebaseDatabasePreDaily().readPreDaily(mAuth.getCurrentUser().getEmail(), new FirebaseDatabasePreDaily.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<PreDaily> books, List<String> keyss) {
+                Log.d(TAG, "DataIsLoaded: data Import");
+                preDailies = new ArrayList<PreDaily>(books);
+                Log.d(TAG, "DataIsLoaded: dailies recived : " + preDailies.get(0).getImageUri());
+                keys = new ArrayList<String>(keyss);
+
+                for (int i = 0; i < preDailies.size(); i++) {
+                    imgURLs.add(Uri.parse(preDailies.get(i).getImageUri()));
+                }
+
+                GridAdapter myAdapter = new GridAdapter(WriteDailyActivity.this, imgURLs);
+                writtableGrid.setAdapter(myAdapter);
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdated() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
+
+        writtableGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent myIntent = new Intent (WriteDailyActivity.this, WriteOnDailyActivity.class);
+                myIntent.putExtra("ImageUri", preDailies.get(position).getImageUri());
+                myIntent.putExtra("Hashtag", preDailies.get(position).getHashtag());
+                myIntent.putExtra("Title", preDailies.get(position).getTitle());
+                myIntent.putExtra("UserEmail", preDailies.get(position).getUserEmail());
+                myIntent.putExtra("UserNick", preDailies.get(position).getUserNick());
+                startActivity(myIntent);
+
+            }
+        });
 
         setupBottomNavigationView();
     }
---
+++ app/src/main/java/com/madcamp/yourdaily/WriteOnDailyActivity.java
@@ -0,0 +1,89 @@
+package com.madcamp.yourdaily;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteOnDailyActivity extends AppCompatActivity {
+    private static final String TAG = "WriteOnDailyActivity";
+
+    private SimpleDraweeView toWriteDrawee;
+    private TextView getTitle;
+    private TextView getHashtag;
+    private EditText contentET;
+    private Button submitButton;
+
+    private Intent mIntent;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_on_daily);
+
+        toWriteDrawee = (SimpleDraweeView) findViewById(R.id.to_write_drawee);
+        getTitle = (TextView) findViewById(R.id.title_content);
+        getHashtag = (TextView) findViewById(R.id.hash_content);
+        contentET = (EditText) findViewById(R.id.content_edittext);
+
+        mIntent = getIntent();
+        mAuth = FirebaseAuth.getInstance();
+
+        toWriteDrawee.setImageURI(Uri.parse(mIntent.getStringExtra("ImageUri")));
+        getTitle.setText(mIntent.getStringExtra("Title"));
+        getHashtag.setText(mIntent.getStringExtra("Hashtag"));
+
+
+        submitButton = (Button)findViewById(R.id.submitDailyButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DailyCard daily = new DailyCard();
+                daily.setContent(contentET.getText().toString());
+                daily.setImageUri(mIntent.getStringExtra("ImageUri"));
+                daily.setTitle(mIntent.getStringExtra("Title"));
+                daily.setUserEmail(mIntent.getStringExtra("UserEmail"));
+                daily.setUserNick(mIntent.getStringExtra("UserNick"));
+                daily.setWriterEmail(mAuth.getCurrentUser().getEmail());
+                daily.setWriterNick("");
+                new FirebaseDatabaseDailyCard().addBook(daily, new FirebaseDatabaseDailyCard.DataStatus() {
+                    @Override
+                    public void DataIsLoaded(List<DailyCard> Dailies, List<String> keys) {
+
+                    }
+
+                    @Override
+                    public void DataIsInserted() {
+                        Toast.makeText(WriteOnDailyActivity.this, "Accepted Successfully", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void DataIsUpdated() {
+
+                    }
+
+                    @Override
+                    public void DataIsDeleted() {
+
+                    }
+                });
+                finish();
+            }
+        });
+    }
+}
